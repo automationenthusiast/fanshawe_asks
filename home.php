b77a0fa184efa7cfdbf9bc5fd16ad0cf060b7c6d
automationenthusiast <shanaksh@amazon.com>
2020-04-19 13:32:34 +0530

    home page updated with new vaigation bar

--- home.php
+++ home.php
@@ -14,26 +14,42 @@ if(!isset($_SESSION['username'])){
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
-        <header>
+        
           <div class = "container">
-                <a href="home.php" class="homelogo" >
-                    <img src="\Fanshawe_asks_logo.jpg" width="170" height="60" title="Fanshawe Asks - One stop solution for all your queries" alt="Fanshawe-asks-logo" class="homelogo" />
-                </a>
                 <nav>
-                   <ol>
-                       <div class="search-box">
-                           <input class="search-txt" name="search" type="text" placeholder="Search your question..">
-                           <a class="search-btn" href="#">
-                               <i class="fa fa-search fa-lg" aria-hidden="true"></i>
-                           </a>
+                   <ul>
+                        <li class="veru">
+                            <a href="home.php" class="homelogo" >
+                                 <img src="\Fanshawe_asks_logo.jpg" width="150" height="50" title="Fanshawe Asks - One stop solution for all your queries" alt="Fanshawe-asks-logo" class="homelogo" />
+                            </a>
+                        </li>    
+                        <li>
+                            <div class="mainbar">
+                               <input class="search-txt" name="search" type="text" placeholder="Search your question..">
+                               <a class="search-btn" href="#">
+                                    <i class="fa fa-search fa-lg" aria-hidden="true"></i>
+                               </a>
+                            </div>
+                        </li>
+                        
+                        <li><a href="#" class ="navlink">Ask</a></li>
+                        <li><a href="#" class = "navlink">Questions</a></li>
+                    
+                        <li><a href="#">Hi, <?php echo $_SESSION['username']; ?>!</a>
+                        <div class ="listlogout">
+                            <ul>
+                               <li><a href="#">Home</a></li>
+                               <li><a href="#">Your profile</a></li>
+                               <li><a href="logout.php">Logout</a></li>
+                            </ul>
                         </div>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Questions</a></li>
-                        <li><h1 class="greetnav">Hi, <?php echo $_SESSION['username']; ?> <a href="logout.php">Logout</a></li>   
-                  </ol>
+                        </li>   
+                  </ul>
                 </nav>
           </div>
-        </header>
+        
+        
+        
     </body>
 <html>
 
